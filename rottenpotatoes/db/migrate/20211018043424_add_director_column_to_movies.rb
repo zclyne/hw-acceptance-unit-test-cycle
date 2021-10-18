d2a17ef8933dbaf74b879056af183ddfef2c5710
Yifan - Macbook Pro <zyfinori@outlook.com>
2021-10-18 15:08:07 -0400

    finished cucumber features

---
+++ rottenpotatoes/db/migrate/20211018043424_add_director_column_to_movies.rb
@@ -0,0 +1,5 @@
+class AddDirectorColumnToMovies < ActiveRecord::Migration
+  def change
+    add_column :movies, :director, :string
+  end
+end
--- rottenpotatoes/db/seeds.rb
+++ rottenpotatoes/db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
-    	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
-    	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
+movies = [{:title => 'Aladdin', :director => 'director1', :rating => 'G', :release_date => '25-Nov-1992'},
+    	  {:title => 'The Terminator', :director => 'director2', :rating => 'R', :release_date => '26-Oct-1984'},
+    	  {:title => 'When Harry Met Sally', :director => 'director3', :rating => 'R', :release_date => '21-Jul-1989'},
       	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
       	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
       	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
